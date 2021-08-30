e55f3dbc8b99fd7e1f224ac0708e230ae3f17c23
RifkiCS29 <rifkipelajar@gmail.com>
2021-08-30 17:10:05 +0800

    User Integration

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -48,7 +48,7 @@ class _FoodPageState extends State<FoodPage> {
                         borderRadius: BorderRadius.circular(8),
                         image: DecorationImage(
                             image: NetworkImage(
-                                'https://i.pinimg.com/474x/8a/f4/7e/8af47e18b14b741f6be2ae499d23fcbe.jpg'),
+                              (context.bloc<UserCubit>().state as UserLoaded).user.picturePath),
                             fit: BoxFit.cover)),
                   )
                 ],
@@ -69,7 +69,20 @@ class _FoodPageState extends State<FoodPage> {
                                       ? defaultMargin
                                       : 0,
                                   right: defaultMargin),
-                              child: FoodCard(e),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Get.to(FoodDetailsPage(
+                                    transaction: Transaction(
+                                      food: e,
+                                      user: (context.bloc<UserCubit>().state as UserLoaded).user
+                                    ),  
+                                    onBackButtonPressed: () {
+                                      Get.back();
+                                    },
+                                  ));
+                                },
+                                child: FoodCard(e)
+                              ),
                             ))
                         .toList(),
                   )
