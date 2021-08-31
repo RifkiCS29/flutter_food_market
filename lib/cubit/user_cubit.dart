6b5f57c394870a4f2dc8bb13eba981b0be2650d9
RifkiCS29 <rifkipelajar@gmail.com>
2021-08-31 23:49:01 +0800

    User Cubit User SignUp and iploadProfilePicture

--- lib/cubit/user_cubit.dart
+++ lib/cubit/user_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:food_market/models/models.dart';
@@ -11,10 +13,32 @@ class UserCubit extends Cubit<UserState> {
   Future<void> signIn(String email, String password) async {
     ApiReturnValue<User> result = await UserServices.signIn(email, password);
 
-    if(result.value != null) {
+    if (result.value != null) {
       emit(UserLoaded(result.value));
     } else {
       emit(UserLoadingFailed(result.message));
+    }
+  }
+
+  Future<void> signUp(User user, String password, {File pictureFile}) async {
+    ApiReturnValue<User> result =
+        await UserServices.signUp(user, password, pictureFile: pictureFile);
+
+    if (result.value != null) {
+      emit(UserLoaded(result.value));
+    } else {
+      emit(UserLoadingFailed(result.message));
+    }
+  }
+
+  Future<void> uploadProfilePicture(File pictureFile) async {
+    ApiReturnValue<String> result =
+        await UserServices.uploadProfilePicture(pictureFile);
+
+    if (result.value != null) {
+      emit(UserLoaded((state as UserLoaded).user.copyWith(
+          picturePath: "http://foodmarket-backend.buildwithangga.id/storage/" +
+              result.value)));
     }
   }
 }
