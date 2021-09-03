2c148f7405b6665d79492cd54ea937011add72aa
RifkiCS29 <rifkipelajar@gmail.com>
2021-09-03 09:45:50 +0800

    Transaction Service Order History

--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -1,15 +1,36 @@
 part of 'services.dart';
 
 class TransactionServices {
-  static Future<ApiReturnValue<List<Transaction>>> getTransactions() async {
-    await Future.delayed(Duration(seconds: 3));
+  static Future<ApiReturnValue<List<Transaction>>> getTransactions(
+      {http.Client client}) async {
+    client ??= http.Client();
 
-    return ApiReturnValue(value: mockTransactions);
+    String url = baseURL + 'transaction';
+
+    var response = await client.get(url, headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${User.token}"
+    });
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Please Try Again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    List<Transaction> transactions = (data['data']['data'] as Iterable)
+        .map((e) => Transaction.fromJson(e))
+        .toList();
+
+    return ApiReturnValue(value: transactions);
   }
 
-  static Future<ApiReturnValue<Transaction>> submitTransaction(Transaction transaction) async {
+  static Future<ApiReturnValue<Transaction>> submitTransaction(
+      Transaction transaction) async {
     await Future.delayed(Duration(seconds: 2));
 
-    return ApiReturnValue(value: transaction.copyWith(id: 123, status: TransactionStatus.pending));
+    return ApiReturnValue(
+        value:
+            transaction.copyWith(id: 123, status: TransactionStatus.pending));
   }
 }
